76021fa39033d6984ee761632b5c1a13245fae1b
AbdullahBhatti1987 <abdullah.bhatti345@gmail.com>
2024-10-14 21:58:53 +0500

    Add repo

--- src/pages/Careers.jsx
+++ src/pages/Careers.jsx
@@ -4,6 +4,7 @@ function Careers() {
   return (
     <div>
       <h1 className='text-black text-center text-2xl'>Careers</h1>
+      <h1 className='text-black text-center text-2xl'>Careers</h1>
     </div>
   )
 }
